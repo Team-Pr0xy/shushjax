bc04f9f29f3f7ffa0b78b358a2bbf736e8e044f7
JC Hulce <jc+git@jchulce.com>
2014-01-28 23:31:44 -0600

    Added jslint to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,18 @@
 module.exports = function(grunt) {
  
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jslint: {
+      all: ['shushjax.js']
+    },
     jshint: {
       all: ['Gruntfile.js', 'shushjax.js']
     }
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jslint');
+  //grunt.loadNpmTasks('grunt-jslint');
   grunt.registerTask('default', 'jshint');
   grunt.registerTask('travis', 'jshint');
  
